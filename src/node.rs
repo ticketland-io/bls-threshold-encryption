d490cf39ddd123ab1bb063828b0a06a69aa80473
ppoliani <ppoliani@gmail.com>
2023-03-07 14:03:45 +0200

    chore: add NodeId

--- src/node.rs
+++ src/node.rs
@@ -1,17 +1,23 @@
 use blsttc::{
   rand::{random, rngs::OsRng}, PublicKey, SecretKey,
 };
+use sn_sdkg::{
+  NodeId
+};
+
 pub struct Node {
+  node_id: NodeId,
   secret_key: SecretKey,
   pub_key: PublicKey,
 }
 
 impl Node {
-  pub fn new() -> Self {
+  pub fn new(node_id: NodeId) -> Self {
     let secret_key = random();
     let pub_key = SecretKey::public_key(&secret_key);
 
     Self {
+      node_id,
       secret_key,
       pub_key,
     }
