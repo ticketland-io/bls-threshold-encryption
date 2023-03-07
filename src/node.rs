a519300c8cf95c7346cbfd1253250396ea253262
ppoliani <ppoliani@gmail.com>
2023-03-07 14:13:09 +0200

    chore: create dkg state for a node

--- src/node.rs
+++ src/node.rs
@@ -1,14 +1,17 @@
+use std::collections::BTreeMap;
+
 use blsttc::{
-  rand::{random, rngs::OsRng}, PublicKey, SecretKey,
+  rand::{random, rngs::OsRng, RngCore}, PublicKey, SecretKey,
 };
 use sn_sdkg::{
-  NodeId
+  NodeId, DkgState
 };
 
 pub struct Node {
   node_id: NodeId,
   secret_key: SecretKey,
-  pub_key: PublicKey,
+  pub pub_key: PublicKey,
+  dkg_state: Option<DkgState>,
 }
 
 impl Node {
@@ -20,6 +23,17 @@ impl Node {
       node_id,
       secret_key,
       pub_key,
+      dkg_state: None,
     }
+  }
+
+  pub fn create_dkg_state(&mut self, threshold: usize, pub_keys: BTreeMap<NodeId, PublicKey>) {
+    self.dkg_state = Some(DkgState::new(
+      self.node_id,
+      self.secret_key.clone(),
+      pub_keys.clone(),
+      threshold,
+      &mut OsRng,
+    ).expect("DKG state to be created"));
   }
 }
