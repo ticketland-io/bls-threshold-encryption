75b5e4f90ea2d4b4f57d83912530cd9d3dc1794c
ppoliani <ppoliani@gmail.com>
2023-03-07 16:05:50 +0200

    feat: implement purge_empty_queues

--- src/net.rs
+++ src/net.rs
@@ -1,4 +1,5 @@
 use std::collections::{BTreeMap, VecDeque};
+use eyre::Result;
 use sn_sdkg::{
   NodeId, DkgSignedVote, DkgState
 };
@@ -46,5 +47,20 @@ impl Net {
       .or_default()
       .push_back(packet);
     }
+  }
+
+  pub fn drain_queued_packets(&mut self) -> Result<()> {
+    while let Some(source) = self.packets.keys().next() {
+      self.purge_empty_queues();
+    }
+
+    Ok(())
+  }
+
+  fn purge_empty_queues(&mut self) {
+    self.packets = core::mem::take(&mut self.packets)
+    .into_iter()
+    .filter(|(_, queue)| !queue.is_empty())
+    .collect();
   }
 }
