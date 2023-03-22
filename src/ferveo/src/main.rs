eb75338130b1d99538249726d7846d1d370b20d3
ppoliani <ppoliani@gmail.com>
2023-03-22 20:32:02 +0200

    feat: implement decrypt in the ferveo example

--- src/ferveo/src/main.rs
+++ src/ferveo/src/main.rs
@@ -1,16 +1,94 @@
-pub use ark_bls12_381::Bls12_381 as EllipticCurve;
-use ferveo::*;
+use ark_bls12_381::Bls12_381 as EllipticCurve;
+use ark_std::{test_rng, rand::rngs::StdRng};
+use ferveo::{
+  PubliclyVerifiableDkg, Message, Params,
+  vss::*,
+};
+use itertools::Itertools;
+use ark_ec::PairingEngine;
+use ark_poly::EvaluationDomain;
 use ferveo_common::{Keypair, ExternalValidator};
 use measure_time::print_time;
+use tpke::{
+  Ciphertext, DecryptionShareSimple, prepare_combine_simple,
+  share_combine_simple,
+};
+
+type Fqk = <EllipticCurve as PairingEngine>::Fqk;
 
 fn main() {
-  setup_dealt_dkg(5, 9);
+  let rng = &mut ark_std::test_rng();
+  let dkg = setup_dealt_dkg(5, 9, rng);
+  
+  let msg = "This is a secret message we want to encrypt using the Pubic key set".as_bytes();
+  let aad: &[u8] = " additional_authenticated_data".as_bytes();
+  let ciphertext = encrypt(dkg, msg, aad);
 }
 
-/// Set up a dkg with enough pvss transcripts to meet the threshold
-fn setup_dealt_dkg(num: u64, shares: u32) {
-  let rng = &mut ark_std::test_rng();
+/// Encrypts using the threshold public key for the given dkg session
+fn encrypt(dkg: PubliclyVerifiableDkg<EllipticCurve>, msg: &[u8], aad: &[u8]) -> Ciphertext<EllipticCurve> {
+  let rng = &mut test_rng();
+  let public_key = dkg.final_key();
   
+  tpke::encrypt::<_, EllipticCurve>(msg, aad, &public_key, rng)
+}
+
+
+fn decrypt(
+  dkg: &PubliclyVerifiableDkg<EllipticCurve>,
+  aad: &[u8],
+  ciphertext: &Ciphertext<EllipticCurve>,
+  validator_keypairs: &[Keypair<EllipticCurve>],
+) -> (
+  PubliclyVerifiableSS<EllipticCurve, Aggregated>,
+  Vec<DecryptionShareSimple<EllipticCurve>>,
+  Fqk,
+) {
+  // Make sure validators are in the same order dkg is by comparing their public keys
+  dkg.validators
+  .iter()
+  .zip_eq(validator_keypairs.iter())
+  .for_each(|(v, k)| {
+    assert_eq!(v.validator.public_key, k.public());
+  });
+
+  let pvss_aggregated = aggregate(dkg);
+
+  let decryption_shares = validator_keypairs
+  .iter()
+  .enumerate()
+  .map(|(validator_index, validator_keypair)| {
+    pvss_aggregated.make_decryption_share_simple(
+      ciphertext,
+      aad,
+      &validator_keypair.decryption_key,
+      validator_index,
+      &dkg.pvss_params.g_inv(),
+    )
+  })
+  .collect::<Vec<DecryptionShareSimple<EllipticCurve>>>();
+
+  let domain = &dkg
+  .domain
+  .elements()
+  .take(decryption_shares.len())
+  .collect::<Vec<_>>();
+  
+  assert_eq!(domain.len(), decryption_shares.len());
+
+  let lagrange_coeffs = prepare_combine_simple::<EllipticCurve>(domain);
+
+  let shared_secret = tpke::share_combine_simple::<EllipticCurve>(
+    &decryption_shares,
+    &lagrange_coeffs,
+  );
+
+  (pvss_aggregated, decryption_shares, shared_secret)
+}
+
+
+/// Set up a dkg with enough pvss transcripts to meet the threshold
+fn setup_dealt_dkg(num: u64, shares: u32, rng: &mut StdRng) -> PubliclyVerifiableDkg<EllipticCurve> {
   // gather everyone's transcripts
   let mut transcripts = vec![];
   for i in 0..num {
@@ -34,13 +112,15 @@ fn setup_dealt_dkg(num: u64, shares: u32) {
     )
     .expect("Setup failed");
   }
+
+  dkg
 }
 
 /// Create a test dkg in state [`DkgState::Init`]
 pub fn setup_dkg(
   validator: usize,
   num: u64,
-  shares_num: u32,
+  shares_num: u32
 ) -> PubliclyVerifiableDkg<EllipticCurve> {
   let keypairs = gen_keypairs(num);
   let validators = gen_validators(&keypairs);
@@ -60,18 +140,14 @@ pub fn setup_dkg(
 }
 
 /// Generate a set of keypairs for each validator
-pub fn gen_keypairs(num: u64) -> Vec<Keypair<EllipticCurve>> {
-  let rng = &mut ark_std::test_rng();
-
+pub fn gen_keypairs(num: u64, rng: &mut StdRng) -> Vec<Keypair<EllipticCurve>> {
   (0..num)
   .map(|_| Keypair::<EllipticCurve>::new(rng))
   .collect()
 }
 
 /// Generate a few validators
-pub fn gen_validators(
-  keypairs: &[ferveo_common::Keypair<EllipticCurve>],
-) -> Vec<ExternalValidator<EllipticCurve>> {
+pub fn gen_validators(keypairs: &[Keypair<EllipticCurve>]) -> Vec<ExternalValidator<EllipticCurve>> {
   (0..keypairs.len())
   .map(|i| ExternalValidator {
     address: format!("validator_{}", i),
